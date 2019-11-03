a564b4eef5fc006fdfda3fb56f996a1a98e7bb7d
Bohdan Havenko <havenko@hotmail.com>
2019-11-03 15:49:48 +0200

    IoC added

--- customer_inquiry/Startup.cs
+++ customer_inquiry/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
+using customer_inquiry.BL;
+using customer_inquiry.DAL;
 using customer_inquiry.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,6 +32,10 @@ namespace customer_inquiry
             var configurationSection = Configuration.GetSection("ConnectionStrings:DefaultConnection");
             services.AddDbContext<CustomerInquiryDbContext>(options => options.UseSqlServer(configurationSection.Value));
             services.AddScoped<ICustomerInquiryDbContext, CustomerInquiryDbContext>();
+
+            RegisterBL(services);
+            RegisterRepositories(services);
+            RegisterAutomapper(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +50,60 @@ namespace customer_inquiry
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+        }
+
+        private void RegisterBL(IServiceCollection services)
+        {
+            var assembly = Assembly.GetAssembly(typeof(IBaseBL));
+
+            var types = assembly.GetTypes();
+            var blInterfaces = assembly.GetTypes()
+                .Where(p => p.GetInterface(typeof(IBaseBL).Name) != null && p.IsInterface && p.IsPublic).ToList();
+
+            foreach (var _interface in blInterfaces)
+            {
+                var type = assembly.GetTypes().FirstOrDefault(p => p.GetInterface(_interface.Name) != null && p.IsPublic);
+                if (type != null)
+                {
+                    services.AddScoped(_interface, type);
+                }
+            }
+        }
+
+        private void RegisterRepositories(IServiceCollection services)
+        {
+            var assembly = Assembly.GetAssembly(typeof(IBaseRepository));
+
+            var types = assembly.GetTypes();
+            var repositoryInterfaces = assembly.GetTypes()
+                .Where(p => p.GetInterface(typeof(IBaseRepository).Name) != null && p.IsInterface && p.IsPublic).ToList();
+
+            foreach (var _interface in repositoryInterfaces)
+            {
+                var type = assembly.GetTypes().FirstOrDefault(p => p.GetInterface(_interface.Name) != null && p.IsPublic);
+                if (type != null)
+                {
+                    services.AddScoped(_interface, type);
+                }
+            }
+        }
+
+        private void RegisterAutomapper(IServiceCollection services)
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                var profileTypes = Assembly.GetExecutingAssembly().GetTypes()
+                        .Where(p => p.IsSubclassOf(typeof(Profile))).ToList();
+
+                foreach (var profileType in profileTypes)
+                {
+                    var obj = (Profile)Activator.CreateInstance(profileType);
+                    mc.AddProfile(obj);
+                }
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
         }
     }
 }
