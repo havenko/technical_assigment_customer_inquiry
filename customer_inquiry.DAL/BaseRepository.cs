fd948b6653b1832afe288cc6454b406c4c9d9c49
Bohdan Havenko <havenko@hotmail.com>
2019-11-03 15:34:52 +0200

    DAL and BL added

---
+++ customer_inquiry.DAL/BaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using customer_inquiry.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customer_inquiry.DAL
+{
+    public class BaseRepository : IBaseRepository
+    {
+        protected readonly CustomerInquiryDbContext context;
+
+        public BaseRepository(CustomerInquiryDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
